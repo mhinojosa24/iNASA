5011b922dc1fd2fc9def734972cbf2840197b811
Maximo Hinojosa <maximohinojosa13@gmail.com>
2023-06-10 19:48:42 -0400

    [Update] - refactored api request class init

--- iNASA/iNASA/Network/ApiRequests/ApiRequest.swift
+++ iNASA/iNASA/Network/ApiRequests/ApiRequest.swift
@@ -16,12 +16,12 @@ enum HTTPMethod: String {
 }
 
 class ApiRequest<T> {
-    private(set) var endpoint: Any
+    private(set) var endpoint: EndPoint
     private(set) var method: HTTPMethod
-    private(set) var headers: [String: Any]
+    private(set) var headers: [String: String]
     private(set) var parameters: [String: Any]
     
-    init(endpoint: Any, method: HTTPMethod, headers: [String : Any] = [:], parameters: [String : Any] = [:]) {
+    init(endpoint: EndPoint, method: HTTPMethod, headers: [String : String] = [:], parameters: [String : Any] = [:]) {
         self.endpoint = endpoint
         self.method = method
         self.headers = headers
