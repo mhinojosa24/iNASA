a1f050dec4bc03073049135e62a8d9c64538e2a8
Maximo Hinojosa <maximohinojosa13@gmail.com>
2023-06-11 08:48:26 -0400

    [Add] - created image search view model and method to call api for search images

--- iNASA/iNASA/Search/View Models/ImageSearchVM.swift
+++ iNASA/iNASA/Search/View Models/ImageSearchVM.swift
@@ -6,6 +6,29 @@
 //
 
 import Foundation
+import Combine
 
 
-
+class ImageSearchVM: BaseVM {
+    
+    private var cancellables = Set<AnyCancellable>()
+    var items: [Items]?
+    
+    func callApiToGetSearch(image query: String) {
+        let getSearchImageRequest = GetSearchQuery(query: query)
+        
+        service?.request(getSearchImageRequest)
+            .sink(receiveCompletion: { completion in
+                switch completion {
+                case .failure(let error):
+                    print(error.localizedDescription)
+                case .finished:
+                    print("Finished!!")
+                }
+            }, receiveValue: { [weak self] searchResult in
+                guard let self = self else { return }
+                self.items = searchResult
+            })
+            .store(in: &cancellables)
+    }
+}
