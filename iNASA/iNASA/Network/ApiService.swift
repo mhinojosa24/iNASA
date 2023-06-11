eb9a04cf558292ea007a53a85bfa29f2e99f1a69
Maximo Hinojosa <maximohinojosa13@gmail.com>
2023-06-10 20:15:08 -0400

    [Add] - created network error types & configured data task publisher

--- iNASA/iNASA/Network/ApiService.swift
+++ iNASA/iNASA/Network/ApiService.swift
@@ -29,7 +29,28 @@ class ApiService: Service {
         return Future<T, Error> { [weak self] promise in
             guard let self = self else { return }
             
-            // create data task
+            session?.dataTaskPublisher(for: request)
+                .tryMap { (data, response) -> Data in
+                    guard let httpResponse = response as? HTTPURLResponse, 200...299 ~= httpResponse.statusCode else { throw NetworkError.responseError }
+                    return data
+                }
+                .decode(type: T.self, decoder: JSONDecoder())
+                .receive(on: RunLoop.main)
+                .sink(receiveCompletion: { completion in
+                    if case let .failure(error) = completion {
+                        switch error {
+                        case let decodingError as DecodingError:
+                            promise(.failure(decodingError))
+                        case let apiError as NetworkError:
+                            promise(.failure(apiError))
+                        default:
+                            promise(.failure(NetworkError.unknown))
+                        }
+                    }
+                }, receiveValue: {
+                    promise(.success($0))
+                })
+                .store(in: &self.cancellable)
         }
     }
     
---
+++ iNASA/iNASA/Network/NetworkError.swift
@@ -0,0 +1,27 @@
+//
+//  NetworkError.swift
+//  iNASA
+//
+//  Created by Maximo Hinojosa on 6/10/23.
+//
+
+import Foundation
+
+enum NetworkError: Error {
+    case invalidURL
+    case responseError
+    case unknown
+}
+
+extension NetworkError: LocalizedError {
+    var errorDescription: String? {
+        switch self {
+        case .invalidURL:
+            return NSLocalizedString("Invalid URL", comment: "Invalid URL")
+        case .responseError:
+            return NSLocalizedString("Unexpected status code", comment: "Invalid response")
+        case .unknown:
+            return NSLocalizedString("Unknown error", comment: "Unknown error")
+        }
+    }
+}
