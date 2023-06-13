69c4d7ce1e2a28a2f6f6bf2d0d8ffab6ca877a55
Maximo Hinojosa <maximohinojosa13@gmail.com>
2023-06-13 13:37:16 -0400

    [Update] - injected UISearchBarDelegate & utilizing to combine to have reactive keystroke events

--- iNASA/iNASA/Search/View Controllers/ImageSearchVC.swift
+++ iNASA/iNASA/Search/View Controllers/ImageSearchVC.swift
@@ -6,23 +6,50 @@
 //
 
 import UIKit
+import Combine
 
-class tableViewDiffableDataSource: UITableViewDiffableDataSource<String?, Item> {}
+class TableViewDiffableDataSource: UITableViewDiffableDataSource<String?, Item> {}
 
 class ImageSearchVC: UIViewController {
-
+    
+    // MARK: - IBOutlets
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
     
+    // MARK: - Published Variables
+    @Published var keyStroke: String = ""
+    
+    // MARK: - Constants & Variables
     let viewModel = ImageSearchVM()
+    var subscribers = Set<AnyCancellable>()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        configureUI()
+        
+        configureTableView()
+        setupObservers()
     }
     
-    func configureUI() {
+    func configureTableView() {
         let itemCell = UINib(nibName: String(describing: ItemCell.self), bundle: nil)
         tableView.register(itemCell, forCellReuseIdentifier: ItemCell.reuseIdentifier)
+    }
+    
+    private func setupObservers() {
+        $keyStroke.receive(on: RunLoop.main)
+            .sink { keyWordValue in
+                print(keyWordValue)
+            }
+            .store(in: &subscribers)
+    }
+}
+
+extension ImageSearchVC: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        self.keyStroke = searchText
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        self.keyStroke = ""
     }
 }
