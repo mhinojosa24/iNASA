2d0826f54ef184338a55ef8b95e50de697f3fe2a
Maximo Hinojosa <maximohinojosa13@gmail.com>
2023-06-13 08:41:32 -0400

    [Update] - created logic to populate table view data source

--- iNASA/iNASA/Search/View Models/ImageSearchVM.swift
+++ iNASA/iNASA/Search/View Models/ImageSearchVM.swift
@@ -12,12 +12,17 @@ import Combine
 class ImageSearchVM: BaseVM {
     
     private var cancellables = Set<AnyCancellable>()
-    var items: [Items]?
+    var tableViewDataSource: [ItemCellModel]?
     
     func callApiToGetSearch(image query: String) {
         service?.request(GetSearchQuery(query: query), completionHandler: { items, error in
             if let error = error {
                 print(error.localizedDescription)
+            }
+            if let items = items {
+                items.compactMap({ ItemCellModel(imageURL: $0.links?.first?.href, title: $0.data?.first?.title, description: $0.data?.first?.description) }).forEach({ item in
+                    self.tableViewDataSource?.append(item)
+                })
             }
         })
     }
--- iNASA/iNASA/Search/Views/ItemCell.swift
+++ iNASA/iNASA/Search/Views/ItemCell.swift
@@ -7,20 +7,32 @@
 
 import UIKit
 
-class ItemCell: UITableViewCell {
+struct ItemCellModel {
+    var imageURL: String?
+    var title: String?
+    var description: String?
+}
 
+class ItemCell: UITableViewCell {
     
     @IBOutlet weak var itemImageView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
+    
+    static var reuseIdentifier: String { String(describing: self) }
+    
+    var model: ItemCellModel? {
+        didSet {
+            setup()
+        }
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    func setup() {
+        
     }
     
 }
