582bbed61645087252f5efea2ba2abdf746e08e9
Maximo Hinojosa <maximohinojosa13@gmail.com>
2023-06-15 11:05:10 -0400

    [Add] - created Unit tests

--- iNASA/iNASATests/iNASATests.swift
+++ iNASA/iNASATests/iNASATests.swift
@@ -9,28 +9,111 @@ import XCTest
 @testable import iNASA
 
 final class iNASATests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    var urlSession: URLSession!
+    var mockService: MockService!
+    let req = MockApiRequest(query: "eclipse")
+    
+    let mockString =
+    """
+    {
+        "collection": {
+            "version": "1.0",
+            "href": "http://images-api.nasa.gov/search?q=eclipse&media_type=image",
+            "items": [
+                {
+                    "href": "https://images-assets.nasa.gov/image/PIA10551/collection.json",
+                    "data": [
+                        {
+                            "center": "JPL",
+                            "title": "Enceladus in Eclipse",
+                            "nasa_id": "PIA10551",
+                            "date_created": "2009-01-07T13:50:41Z",
+                            "keywords": [
+                                "Enceladus",
+                                "Cassini-Huygens"
+                            ],
+                            "media_type": "image",
+                            "description_508": "Enceladus in Eclipse",
+                            "secondary_creator": "NASA/JPL/Space Science Institute",
+                            "description": "Enceladus in Eclipse"
+                        }
+                    ],
+                    "links": [
+                        {
+                            "href": "https://images-assets.nasa.gov/image/PIA10551/PIA10551~thumb.jpg",
+                            "rel": "preview",
+                            "render": "image"
+                        }
+                    ]
+                }
+            ]
         }
     }
+    """
+    
 
+    func testRequestFactory() throws {
+        let r = GetSearchQuery(query: "mars")
+        XCTAssertTrue(r.endpoint.url.scheme == "https")
+        XCTAssertTrue(r.endpoint.url.path() == "/search")
+        XCTAssertTrue(r.endpoint.url.query() == "q=mars&media_type=image")
+        XCTAssertTrue(r.endpoint.url.host() == Bundle.main.infoDictionary?["SERVER_URL"] as? String ?? "")
+    }
+    
+
+    
+    override func setUp() {
+        let config = URLSessionConfiguration.ephemeral
+        config.protocolClasses = [MockURLProtocol.self]
+        mockService = MockService()
+    }
+    
+    override func tearDown() {
+        urlSession = nil
+        mockService = nil
+        super.tearDown()
+    }
+    
+    func testGetImageSuccess() throws {
+        let response = HTTPURLResponse(url: req.url,
+                                       statusCode: 200,
+                                       httpVersion: nil,
+                                       headerFields: ["Content-Type": "application/json"])!
+        
+        let mockData: Data = Data(mockString.utf8)
+        
+        MockURLProtocol.requestHandler = { request in
+            return (response, mockData)
+        }
+        
+        let expectation = XCTestExpectation(description: "response")
+        mockService.request(req) { items, error in
+            if let error = error {
+                XCTAssertThrowsError(error)
+            }
+            
+            if let items = items {
+                XCTAssertEqual(items.first?.data?.first?.nasaId, "PIA10551")
+                expectation.fulfill()
+            }
+        }
+    }
 }
+
+class MockService: Service, Mockable {
+    func request<T: Decodable>(_ resource: ApiRequest<T>, completionHandler: @escaping ApiHandler<T>)  {
+        return loadJson(filename: "MockData",
+                        extensionType: .json,
+                        type: T.self) { result in
+            switch result {
+            case .success(let items):
+                completionHandler(items, nil)
+            case .failure(let error):
+                completionHandler(nil, error)
+            }
+        }
+    }
+}
+
+
+
